ce30a97845d76fca0f39d4eac6bf9e80f4febc52
ShubhamRameshdaga <subhamdaga7161@gmail.com>
2023-07-28 19:28:35 +0530

    npm start error

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,13 @@ import tranRoute from "./router/transactionRoutes.js";
 import cors from "cors";
 import path from "path";
 
+import { fileURLToPath } from "url";
+
 dotenv.config();
 
 connectDB();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 

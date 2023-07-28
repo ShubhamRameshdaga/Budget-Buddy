2f9156c21e7c180cb8d1c4134116ed3b713ea94e
ShubhamRameshdaga <subhamdaga7161@gmail.com>
2023-07-28 22:33:25 +0530

    add start

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ import connectDB from "./config/db.js";
 import authRoute from "./router/authRoute.js";
 import tranRoute from "./router/transactionRoutes.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -17,9 +22,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/auth", tranRoute);
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("/", (req, res) => {
-  res.send("Hello World from the SHubham");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 const PORT = process.env.port || 8080;

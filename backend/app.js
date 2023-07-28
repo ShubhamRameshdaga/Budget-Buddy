8f092bbcc8d7cc8f02aa5364b9c9c389835a25d5
ShubhamRameshdaga <subhamdaga7161@gmail.com>
2023-07-28 18:32:58 +0530

    added path to client from app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import authRoute from "./router/authRoute.js";
 import tranRoute from "./router/transactionRoutes.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -17,12 +18,13 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/auth", tranRoute);
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("/", (req, res) => {
-  res.send("Hello World from the SHubham");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.port;
 
 app.listen(PORT, () => {
   console.log(

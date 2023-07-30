3c2ebab2be0410248806a5b6baaf3e2da1c2e21d
ShubhamRameshdaga <subhamdaga7161@gmail.com>
2023-07-30 12:56:59 +0530

    . error

--- controller/authController.js
+++ controller/authController.js
@@ -52,12 +52,11 @@ export const registerController = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    res.status(500),
-      send({
-        success: false,
-        message: "Error in Registration",
-        error,
-      });
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
   }
 };
 
